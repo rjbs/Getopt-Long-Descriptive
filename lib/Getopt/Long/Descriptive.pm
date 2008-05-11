a4b79d8aa67bcc3e1b9cabe1fac53f81763f5587
Hans Dieter Pearcey <hdp@pobox.com>
2008-05-11 07:46:01 -0400

    [bump] 0.073 -> 0.074

--- lib/Getopt/Long/Descriptive.pm
+++ lib/Getopt/Long/Descriptive.pm
@@ -13,11 +13,11 @@ Getopt::Long::Descriptive - Getopt::Long with usage text
 
 =head1 VERSION
 
-Version 0.073
+Version 0.074
 
 =cut
 
-our $VERSION = '0.073';
+our $VERSION = '0.074';
 
 =head1 DESCRIPTION
 
